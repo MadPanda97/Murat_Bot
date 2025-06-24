ced1319f7b74692a564a3c9d979ffcf285c13363
arnuraffise <a.katayev@affise.com>
2025-06-24 16:35:23 +0700

    fix: new branch

--- main.go
+++ main.go
@@ -155,7 +155,7 @@ func main() {
 
 				msg := tgbotapi.NewMessage(chatID, `💳 Оплата:
 Переведи 4 900 ₸ на Kaspi:
-https://pay.kaspi.kz/pay/s2ompo9l
+https://pay.kaspi.kz/pay/s2ompo9l 
 
 📝 В комментарии укажи:
 "ЗАВТРАК + Имя"
